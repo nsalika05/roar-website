2d3e6f45ab5fb26139408fb19e7492c9e177215b
Alika Nicosia-Stone <nsalika05@gmail.com>
2026-07-08 15:24:16 -0500

    Complete website and add support page

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 20) {
+
+        header.classList.add("scrolled");
+
+    } else {
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
